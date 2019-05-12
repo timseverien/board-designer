da3172038742da4e0f5119959c598b9f64132b1b
Tim Severien <timseverien@gmail.com>
2019-05-12 10:30:49 +0200

    Align dots to grid

--- src/scripts/core/Math.js
+++ src/scripts/core/Math.js
@@ -2,4 +2,8 @@ export default {
 	lerp(a, b, t) {
 		return a + t * (b - a);
 	},
+
+	floorEvery(n, step) {
+		return Math.floor(n / step) * step;
+	},
 };
--- src/scripts/generators/DotGraphic.js
+++ src/scripts/generators/DotGraphic.js
@@ -4,28 +4,35 @@ import MathUtils from '../core/Math';
 export default class DotGraphicGenerator {
 	constructor() {
 		this.backgroundColor = '#000';
-		this.dotSizeMin = 1 / 256;
-		this.dotSizeMax = 1 / 32;
+		this.dotCount = 512;
+		this.gridCount = 8;
 	}
 
 	// eslint-disable-next-line class-methods-use-this
 	generate(context) {
 		const { height, width } = context.canvas;
-		const size = 2 * Math.min(width, height);
 		const palette = colors[Math.floor(colors.length * Math.random())];
+		const gridSize = Math.min(width, height) / this.gridCount;
+
+		const radiusMin = gridSize / 32;
+		const radiusMax = gridSize / 3;
+
+		const offset = 0.5 * gridSize;
 
 		context.fillStyle = palette.pop();
 		context.fillRect(0, 0, width, height);
 
-		for (let i = 0; i < 1024; i++) {
-			const x = MathUtils.lerp(0, width, Math.random());
-			const y = MathUtils.lerp(0, height, Math.random());
-			const radius = size * MathUtils.lerp(this.dotSizeMin, this.dotSizeMax, Math.random());
+		for (let i = 0; i < this.dotCount; i++) {
+			const t = (this.dotCount - 1 - i) / (this.dotCount - 1);
+			const x = offset + MathUtils.floorEvery(MathUtils.lerp(0, width, Math.random()), gridSize);
+			const y = offset + MathUtils.floorEvery(MathUtils.lerp(0, height, Math.random()), gridSize);
+			const radius = MathUtils.lerp(radiusMin, radiusMax, t);
+			const radiusRounded = MathUtils.floorEvery(radius, radiusMin);
 
 			context.fillStyle = palette[Math.floor(palette.length * Math.random())];
 
 			context.beginPath();
-			context.arc(x, y, radius, 0, 2 * Math.PI);
+			context.arc(x, y, radiusRounded, 0, 2 * Math.PI);
 			context.fill();
 		}
 	}
